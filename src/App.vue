d402ccc5895d0887ab120683da078ed36919ccb7
ilmariov <72055958+ilmariov@users.noreply.github.com>
2023-02-11 17:15:29 -0500

    json backend file loaded

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 import Header from './components/Header.vue'
 import Tasks from './components/Tasks.vue'
 import AddTask from './components/AddTask.vue'
+import { rmSync } from 'fs'
 
 export default {
   name: 'App',
@@ -40,29 +41,17 @@ export default {
     }, 
     toggleReminder(id) {
       this.tasks = this.tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)
+    },
+    async fetchTasks() {
+      const res = await fetch('http://localhost:3000/tasks')
+
+      const data = await res.json()
+
+      return data
     }
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'March 1st at 2:30pm',
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        day: 'March 3rd at 1:30pm',
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'March 1st at 11:00am',
-        reminder: false,
-      },
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   }
 }
 </script>
